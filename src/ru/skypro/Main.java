b1358a829581acb57c9ddd38b7d18f2e40aae87b
Андрей <79538233369@ya.ru>
2022-05-09 18:52:42 +0500

    5.4

--- src/ru/skypro/Main.java
+++ src/ru/skypro/Main.java
@@ -5,23 +5,16 @@ import java.util.Arrays;
 public class Main {
 
     public static void main(String[] args) {
-        int[] arr = generateRandomArray();
+        char[] reverseFullName = {'n', 'a', 'v', 'I', ' ', 'V', 'o', 'n', 'a', 'v', 'i'};
+        for (int i = reverseFullName.length - 1; i >= 0; i--) {
+            System.out.println(reverseFullName [i]);
+        }
+
+        }
     }
 
-    public static int[] generateRandomArray() {
-        java.util.Random random = new java.util.Random();
-        int[] costs = new int[30];
-        float sum = 0f;
-        float average = 0f;
-        for (int i = 0; i < costs.length; i++) {
-            costs[i] = random.nextInt(100_000) + 100_000;
-            sum += costs[i];
-            average = sum / 30;
-                }
-        System.out.println("The average of all the costs in the month is " + average + " rub. ");
-            return costs;
-    }
-}
+
+
 
 
 
