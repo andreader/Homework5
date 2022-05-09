1527da93e610252004f0cf0fa177c60debf65eae
Андрей <79538233369@ya.ru>
2022-05-09 18:57:13 +0500

    5.4 correct

--- src/ru/skypro/Main.java
+++ src/ru/skypro/Main.java
@@ -5,9 +5,9 @@ import java.util.Arrays;
 public class Main {
 
     public static void main(String[] args) {
-        char[] reverseFullName = {'n', 'a', 'v', 'I', ' ', 'V', 'o', 'n', 'a', 'v', 'i'};
+        char[] reverseFullName = {'n', 'a', 'v', 'I', ' ', 'v', 'o', 'n', 'a', 'v', 'I'};
         for (int i = reverseFullName.length - 1; i >= 0; i--) {
-            System.out.println(reverseFullName [i]);
+            System.out.print(reverseFullName [i]);
         }
 
         }
