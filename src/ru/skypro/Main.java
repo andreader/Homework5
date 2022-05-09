ef446c0b44ed135fc5a18e9cf85069b77823e2b1
Андрей <79538233369@ya.ru>
2022-05-09 18:25:06 +0500

    5.2

--- src/ru/skypro/Main.java
+++ src/ru/skypro/Main.java
@@ -5,19 +5,28 @@ import java.util.Arrays;
 public class Main {
 
     public static void main(String[] args) {
-    int[] arr = generateRandomArray();}
+        int[] arr = generateRandomArray();
+    }
+
     public static int[] generateRandomArray() {
         java.util.Random random = new java.util.Random();
         int[] costs = new int[30];
-        int sum = 0;
+        int cost = 0, minCosts = 200001, maxCosts = -1;
         for (int i = 0; i < costs.length; i++) {
             costs[i] = random.nextInt(100_000) + 100_000;
-                sum += costs[i];
+            if (costs[i] > maxCosts) {
+                maxCosts = costs[i];
+                if (costs[i] < minCosts) {
+                    minCosts = costs[i];
+                }
+            }
         }
-        System.out.print("The total of all the costs in the month is " + sum + " rub");
-        return costs;
-        }
+        System.out.println("The minimum of all the costs in the month is " + minCosts + " rub. ");
+        System.out.println("The maximum of all the costs in the month is " + maxCosts + " rub");
+            return costs;
     }
+}
+
 
 
 
